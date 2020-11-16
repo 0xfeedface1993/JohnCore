cb2664a547904c428a40869bf5efb233c9746132
许庭淞 <virus1993@icloud.com>
2020-11-16 11:01:02 +0800

    修复点击事件在新SDK下无响应的问题

--- Sources/JohnCore/UIView+Tap.swift
+++ Sources/JohnCore/UIView+Tap.swift
@@ -9,33 +9,44 @@
 import UIKit
 
 extension UIView {
-    // 定义手势和闭包关联的Key
-    private struct AssociateKeys {
-        static var gestureKey = "UIView+Extension+gestureKey"
-        static var closureKey = "UIView+Extension+closureKey"
+    // In order to create computed properties for extensions, we need a key to
+    // store and access the stored property
+    fileprivate struct AssociatedObjectKeys {
+        static var tapGestureRecognizer = "MediaViewerAssociatedObjectKey_mediaViewer"
     }
     
-    // 为view添加点击事件
-    @discardableResult
-    public func onTap(_ closure: @escaping () -> ()) -> Self {
-        var gesture = objc_getAssociatedObject(self, &AssociateKeys.gestureKey)
-        if gesture == nil {
-            gesture = UITapGestureRecognizer(target: self, action: #selector(handleActionForTapGesture(_:)))
-            addGestureRecognizer(gesture as! UIGestureRecognizer)
-            isUserInteractionEnabled = true
-            objc_setAssociatedObject(self, &AssociateKeys.gestureKey, gesture, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
-        }
-        objc_setAssociatedObject(self, &AssociateKeys.closureKey, closure, objc_AssociationPolicy.OBJC_ASSOCIATION_COPY)
-        return self
-    }
+    fileprivate typealias Action = (() -> Void)?
     
-    // 点击手势实际调用的函数
-    @objc private func handleActionForTapGesture(_ gesture: UITapGestureRecognizer) {
-        if gesture.state == UIGestureRecognizer.State.recognized {
-            let obj = objc_getAssociatedObject(self, &AssociateKeys.closureKey)
-            if let action = obj as? ()->() {
-                action()
+    // Set our computed property type to a closure
+    fileprivate var tapGestureRecognizerAction: Action? {
+        set {
+            if let newValue = newValue {
+                // Computed properties get stored as associated objects
+                objc_setAssociatedObject(self, &AssociatedObjectKeys.tapGestureRecognizer, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
             }
+        }
+        get {
+            let tapGestureRecognizerActionInstance = objc_getAssociatedObject(self, &AssociatedObjectKeys.tapGestureRecognizer) as? Action
+            return tapGestureRecognizerActionInstance
+        }
+    }
+    
+    // This is the meat of the sauce, here we create the tap gesture recognizer and
+    // store the closure the user passed to us in the associated object we declared above
+    public func onTap(_ action: (() -> Void)?) {
+        self.isUserInteractionEnabled = true
+        self.tapGestureRecognizerAction = action
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture))
+        self.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    // Every time the user taps on the UIImageView, this function gets called,
+    // which triggers the closure we stored
+    @objc fileprivate func handleTapGesture(sender: UITapGestureRecognizer) {
+        if let action = self.tapGestureRecognizerAction {
+            action?()
+        } else {
+            print("no action")
         }
     }
 }
